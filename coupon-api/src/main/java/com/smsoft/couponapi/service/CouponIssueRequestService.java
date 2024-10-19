9755464974644ee77601a5056ba4533331c02f0d
kimsumin <tnals1564@naver.com>
2024-10-19 19:22:24 +0900

    Concurrency issue resolution - redis lock

--- coupon-api/src/main/java/com/smsoft/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/smsoft/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.smsoft.couponapi.service;
 
 import com.smsoft.couponapi.controller.dto.CouponIssueRequestDto;
+import com.smsoft.couponcore.component.DistributeLockExecutor;
 import com.smsoft.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -11,10 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () -> {
+            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        });
         logger.info("Issue request completed. couponId: {}, userId: {}", requestDto.couponId(), requestDto.userId());
     }
 }
