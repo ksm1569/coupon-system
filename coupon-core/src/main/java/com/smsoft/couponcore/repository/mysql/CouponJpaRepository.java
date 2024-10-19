7ed884b63b4848063009b062aa8cffd378ca9395
kimsumin <tnals1564@naver.com>
2024-10-19 19:26:27 +0900

    Concurrency issue resolution - mysql lock

--- coupon-core/src/main/java/com/smsoft/couponcore/repository/mysql/CouponJpaRepository.java
+++ coupon-core/src/main/java/com/smsoft/couponcore/repository/mysql/CouponJpaRepository.java
@@ -1,7 +1,15 @@
 package com.smsoft.couponcore.repository.mysql;
 
 import com.smsoft.couponcore.model.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.id = :id")
+    Optional<Coupon> findCouponWithLock(long id);
 }
